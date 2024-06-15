6479c2244cc9290962e28e4fce37748a2f938489
Bruno Klein <bruno.klein@cwi.com.br>
2024-06-15 18:23:24 -0300

    install react native icons

---
+++ src/colors/color-palette.jsx
@@ -0,0 +1,9 @@
+export const colorPalette = {
+  lightPrimaryColor: '#32CD32',
+  darkPrimaryColor: '#FFD700',
+  lightInactiveColor: '#9DA8C3',
+  darkInactiveColor: '#AEB7D6',
+  white: '#FFFFFF',
+  black: '#000000',
+  waterBlue: '#4166F5',
+};
