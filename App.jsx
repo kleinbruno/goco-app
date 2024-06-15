3893638cc77e6449093f9996f717cab41b72c7fa
Bruno Klein <bruno.klein@cwi.com.br>
2024-06-15 16:27:45 -0300

    add tabbar

--- App.jsx
+++ App.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
-import { useColorScheme, View } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from './src/screens';
+
+const Tab = createBottomTabNavigator();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <NavigationContainer>
-      <View />
+      <Tab.Navigator
+        initialRouteName={'Home'}
+        // activeColor={colorPalette.lightPrimaryColor}
+        // inactiveColor={colorPalette.lightInactiveColor}
+        // barStyle={{ backgroundColor: colorPalette.white }}
+      >
+        <Tab.Screen
+          name={'Home'}
+          component={HomeScreen}
+          // options={{
+          //   tabBarIcon: ({ focused }) => (
+          //     <RealIcon
+          //       iconName={'checkmark-done-sharp'}
+          //       iconSize={25}
+          //       color={getColor(focused)}
+          //     />
+          //   ),
+          // }}
+        />
+        <Tab.Screen
+          name={'2'}
+          component={HomeScreen}
+          // options={{
+          //   tabBarIcon: ({ focused }) => (
+          //     <RealIcon
+          //       iconName={'checkmark-done-sharp'}
+          //       iconSize={25}
+          //       color={getColor(focused)}
+          //     />
+          //   ),
+          // }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
